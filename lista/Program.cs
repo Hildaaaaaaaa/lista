37616af6f0b334f53a6d49c71a4aa891cb2fd23d
Hilda <glimrot.hilda@elev.ga.ntig.se>
2023-04-27 11:08:17 +0200

    ijn

---
+++ lista/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<string> ListVaror = new List<string> ()  {"mjölk", "socker", "grillkorv", "senap"};
+
+Console.WriteLine("Inköpslista: ");
+
+Console.WriteLine(ListVaror);
+
+Console.WriteLine("Vad för vara vill du lägga till?");
+
+
+ListVaror.Add(Console.ReadLine());
+
+Console.ReadLine();
